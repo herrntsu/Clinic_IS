d6d5128bbb6572edec5e5c68ebd3d327a5d73e46
herrntsu <brentharveyrull@gmail.com>
2024-05-16 17:32:43 +0800

    added logo, font, and grid

--- login-page.php
+++ login-page.php
@@ -5,32 +5,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:100,200,300,400" rel="stylesheet"> 
     <title>Login</title>
 </head>
 
 <body>
     <div class="container">
         <p>Please fill in your email and password.</p>
-        <form action="" method="post">
-            <div class="form-group1">
+        <form action="" method="post" class = "form-group">
+            <div class="formgroup">
                 <label>Email Address:</label>
                 <input type="email" name="email" id="user-email" required />
             </div>
-            <div class="form-group2">
+            <div class="formgroup">
                 <label>Password</label>
-                <input type="password" name="password" class="form-control" required>
+                <input type="password" name="password" class="user-password" required>
             </div>
-            <div class="form-group3">
-                <input type="submit" name="submit" class="btn btn-primary" value="Submit">
+            <div class="formgroup">
+                <input type="submit" name="submit" class="submit-btn" value="Submit">
             </div>
-    </div>
-</body>
 
-</html>
-</form>
-</div>
-</div>
-</div>
+            <img src = "logo.jpg">
+    </div>
 </body>
 
 </html>
