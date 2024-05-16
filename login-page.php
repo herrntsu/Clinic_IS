1ebfef76aff69819ffc0563a05000fb67506252c
herrntsu <brentharveyrull@gmail.com>
2024-05-16 22:10:27 +0800

    fixed flex

--- login-page.php
+++ login-page.php
@@ -5,30 +5,38 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css?family=Montserrat:100,200,300,400" rel="stylesheet"> 
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:100,200,300,400" rel="stylesheet">
     <title>Login</title>
 </head>
 
 <body>
+    <!-- start of container class -->
     <div class="container">
-        <p>Please fill in your email and password.</p>
-        <img src = "logo.jpg">
-        <form action="" method="post" class = "form-group">
-            <div class="formgroup">
-                <label>Email Address:</label>
-                <input type="email" name="email" id="user-email" required />
-            </div>
-            <div class="formgroup">
-                <label>Password</label>
-                <input type="password" name="password" class="user-password" required>
-            </div>
-            <div class="formgroup">
-                <input type="submit" name="submit" class="submit-btn" value="Submit">
-            </div>
 
-            
+
+        <!-- start of form-group -->
+        <form action="" method="post" class="form-group">
+            <img src="logo.jpg">
+            <h2>Login to AnchorMed Clinic.</h2>
+            <div class="form-group">
+                <label>Email Address:</label>
+                <input type="email" name="email" id="user-email" required/>
+                
+                <label>Password</label>
+                <input type="password" name="password" class="user-password" placeholder="Password" required>
+                <input type="submit" name="submit" class="submit-btn" value="Sign in">
+                <p>By clicking sign in, you are agreeing to the <a href="terms&conditions.php">Terms & Conditions</a></p>
+            </div>
+        </form>
+        
+        <!-- end of form-group -->
+        
+    </div>
+    <!-- end of container class -->
+
+    <!-- start of footer class -->
+    <div class="footer">
+        <div class="item1"></div>
     </div>
 </body>
 
