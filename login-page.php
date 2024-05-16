1e08a073738360833669b8ddc1c819d29a0a34c2
herrntsu <brentharveyrull@gmail.com>
2024-05-17 01:19:05 +0800

    asdasdasdasd

--- login-page.php
+++ login-page.php
@@ -19,13 +19,14 @@
             <img src="logo.jpg">
             <h2>Login to AnchorMed Clinic.</h2>
             <div class="form-group">
-                <label>Email Address:</label>
-                <input type="email" name="email" id="user-email" required/>
+                <label>E-mail Address:</label>
+                <input type="email" name="email" id="user-email" placeholder="E-mail"required/>
                 
-                <label>Password</label>
+                <label>Password:</label>
                 <input type="password" name="password" class="user-password" placeholder="Password" required>
                 <input type="submit" name="submit" class="submit-btn" value="Sign in">
                 <p>By clicking sign in, you are agreeing to the <a href="terms&conditions.php">Terms & Conditions</a></p>
+                <style></style>
             </div>
         </form>
         
@@ -34,6 +35,10 @@
     </div>
     <!-- end of container class -->
 
+
+    <div class="redbl">
+        <!-- <img src="Blue Block.png" alt=""> -->
+    </div>
     <!-- start of footer class -->
     <div class="footer">
         <div class="item1"></div>
