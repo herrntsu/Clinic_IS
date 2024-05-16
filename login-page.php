6c1f23e44dbed35301fcd8e3aa3b386bf92ecd1a
herrntsu <brentharveyrull@gmail.com>
2024-05-16 16:56:01 +0800

    redo css

--- login-page.php
+++ login-page.php
@@ -9,18 +9,18 @@
 </head>
 
 <body>
-<p>Please fill in your email and password.</p>
     <div class="container">
+        <p>Please fill in your email and password.</p>
         <form action="" method="post">
-            <div class="form-group">
+            <div class="form-group1">
                 <label>Email Address:</label>
                 <input type="email" name="email" id="user-email" required />
             </div>
-            <div class="form-group">
+            <div class="form-group2">
                 <label>Password</label>
                 <input type="password" name="password" class="form-control" required>
             </div>
-            <div class="form-group">
+            <div class="form-group3">
                 <input type="submit" name="submit" class="btn btn-primary" value="Submit">
             </div>
     </div>
